e4c6a2df44f910a6139afc39ea185b6cf234d0ee
Vincent M. Visconti <vviscont@SPF.local>
2016-12-28 16:01:44 -0500

    implementing Auth

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,58 +12,9 @@ $(document).ready(function() {
 
   var databaseRef = firebase.database();
 
-
-
-
-/**
- * Initializes the app.
- */
-// var initApp = function() {
-
-
-//   document.getElementById('sign-in-with-redirect').addEventListener(
-//       'click', signInWithRedirect);
-//   document.getElementById('sign-in-with-popup').addEventListener(
-//       'click', signInWithPopup);
-//   document.getElementById('sign-out').addEventListener('click', function() {
-//     firebase.auth().signOut();
-//   });
-//   document.getElementById('delete-account').addEventListener(
-//       'click', function() {
-//         deleteAccount();
-//       });
-// };
-
-// window.addEventListener('load', initApp);
-
-
-
-
-
-
-
-
-
-
- //  var uiConfig = {
- //    signInSuccessUrl: "file:///C:/Users/vviscont/Google%20Drive/RCBprojects/TrainTracker/index.html",
- //    signInOptions: [
- //      // Leave the lines as is for the providers you want to offer your users.
- //      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
- //      firebase.auth.GithubAuthProvider.PROVIDER_ID
- //    ],
- //    // Terms of service url.
- //    tosUrl: "file:///C:/Users/vviscont/Google%20Drive/RCBprojects/TrainTracker/tos.html"
- //  };
-
- //  // Initialize the FirebaseUI Widget using Firebase.
- //  var ui = new firebaseui.auth.AuthUI(firebase.auth());
- //  // The start method will wait until the DOM is loaded.
- //  ui.start("#firebaseui-auth-container", uiConfig);
-
 'use strict';
 
-// Initializes FriendlyChat.
+// Initialize
 function trainTracker() {
 
   // Shortcuts to DOM Elements.
@@ -136,51 +87,6 @@ trainTracker.prototype.onAuthStateChanged = function(user) {
 window.onload = function() {
   window.trainTracker = new trainTracker();
 };
-
-
-
-
-
-        // firebase.auth().onAuthStateChanged(function(user) {
-        //   if (user) {
-        //     // User is signed in.
-        //     var displayName = user.displayName;
-        //     var email = user.email;
-        //     var emailVerified = user.emailVerified;
-        //     var photoURL = user.photoURL;
-        //     var uid = user.uid;
-        //     var providerData = user.providerData;
-        //     user.getToken().then(function(accessToken) {
-        //       document.getElementById("sign-in-status").textContent = "Signed in";
-        //       document.getElementById("sign-in").textContent = "Sign out";
-        //       document.getElementById("account-details").textContent = JSON.stringify({
-        //         displayName: displayName,
-        //         email: email,
-        //         emailVerified: emailVerified,
-        //         photoURL: photoURL,
-        //         uid: uid,
-        //         accessToken: accessToken,
-        //         providerData: providerData
-        //       }, null, "  ");
-        //     });
-        //   } else {
-        //     // User is signed out.
-        //     document.getElementById("sign-in-status").textContent = "Signed out";
-        //     document.getElementById("sign-in").textContent = "Sign in";
-        //     document.getElementById("account-details").textContent = "null";
-        //   }
-        // }, function(error) {
-        //   console.log(error);
-        // });
-      // };
-
-      // window.addEventListener("load", function() {
-      //   initApp()
-      // });
-
-
-
-
 
     $("#first-train-time-input").timepicker({ timeFormat: "HH:mm", interval: 5 });
 
